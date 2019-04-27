0be34de4099ea277d9d30023d3776af04bd167cd
XenonLab-Studio <xenonlab.develop@gmail.com>
2019-04-27 20:25:48 +0200

    Added variables for verbs and player commands

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 /*
- * *****************************************************
- * * © 2019 Stefano Peris <xenonlab.develop@gmail.com> *
- * *****************************************************
- * 
+ *****************************************************
+ * © 2019 Stefano Peris <xenonlab.develop@gmail.com> *
+ *****************************************************
+ *
  * Released under the GNU/GPL 3.0 license
  *
  * github: <https://github.com/XenonLab-Studio/The-Great-Adventure>
@@ -28,6 +28,22 @@
  *          |
  */
 
+var verbAliasMap = map[string][]string{
+	"GO NORTH":   {"N", "NORTH"},
+	"GO SOUTH":   {"S", "SOUTH"},
+	"GO WEST":    {"W", "WEST"},
+	"GO EAST":    {"E", "EAST"},
+	"GO IN":      {"IN", "INSIDE", "ENTER"},
+	"GO OUT":     {"OUT", "OUTSIDE", "LEAVE"},
+	"GO UP":      {"UP"},
+	"GO DOWN":    {"DOWN"},
+	"LOOK AT":    {"EXAMINE", "INSPECT", "X"},
+	"LOOK UNDER": {"LOOK BENEATH", "LOOK BELOW"},
+	"TAKE":       {"PICK UP", "GET"},
+	"DROP":       {"THROW"},
+	"INVENTORY":  {"I"},
+	"WAIT":       {"Z"},
+}
 
 package main
 
